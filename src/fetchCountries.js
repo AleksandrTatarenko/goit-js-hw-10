04606174e7f637f3198dc668ddbcb34bf88f1e79
AleksandrTatarenko <78167369+AleksandrTatarenko@users.noreply.github.com>
2022-01-06 15:11:21 +0200

    hw 10

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,7 @@
 const searchParams = 'name,capital,population,flags,languages';
 const baseUrl = 'https://restcountries.com/v3.1/name';
 function fetchCountries(name) { 
-    fetch(`${baseUrl}/${name}?fields=${searchParams}`)
+    return fetch(`${baseUrl}/${name}?fields=${searchParams}`)
     .then(response => { return response.json() })  
 };
 export default { fetchCountries };
